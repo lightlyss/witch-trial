e92878244e0daaabe9f8c5020d940044953aefe5
Chara <37601555+lightlyss@users.noreply.github.com>
2019-06-24 03:31:00 -0400

    prototype basic motion

--- Assets/Scripts/ctrl.cs
+++ Assets/Scripts/ctrl.cs
@@ -3,21 +3,29 @@ using UnityEngine;
 public class ctrl : MonoBehaviour {
   private Animator ani;
   private Rigidbody2D rb;
+  private SpriteRenderer sr;
 
   void Start() {
     ani = this.GetComponent<Animator>();
     rb = this.GetComponent<Rigidbody2D>();
+    sr = this.GetComponent<SpriteRenderer>();
   }
 
   void Update() {
-    var horizontal = Input.GetAxis("Horizontal");
-    if (horizontal > 0) {
-      rb.velocity = new Vector2(2, rb.velocity.y);
-    } else if (horizontal < 0) {
-      rb.velocity = new Vector2(-2, rb.velocity.y);
+    var xdir = Input.GetAxisRaw("Horizontal");
+    var v = rb.velocity;
+
+    if (xdir > 0) {
+      ani.SetInteger("xdir", 1);
+      sr.flipX = false;
+      rb.velocity = new Vector2(5, v.y);
+    } else if (xdir < 0) {
+      ani.SetInteger("xdir", -1);
+      sr.flipX = true;
+      rb.velocity = new Vector2(-5, v.y);
     } else {
-      rb.velocity = new Vector2(0, rb.velocity.y);
+      ani.SetInteger("xdir", 0);
+      rb.velocity = new Vector2(0, v.y);
     }
-    ani.SetInteger("xvel", (int) rb.velocity.x);
   }
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Witch Trial
+Experimental fan project for Twilight Frontier's Grief Syndrome.
+
+## Requirements
+Built and tested on:
+- Windows 7, 64 bit architecture
+- Unity `2018.3.14f1`
+- .NET 3.5 Scripting Runtime
+- Mono Scripting Backend
+- .NET 2.0 API
