a28df05da72cd1c0c492b61a251e4ccee5af52dc
Chara <37601555+lightlyss@users.noreply.github.com>
2019-06-29 17:17:06 -0400

    finish jumping, adjust camera

---
+++ Assets/Scripts/cam.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class cam : MonoBehaviour {
+  public float dampTime = 0.15f;
+  public Transform target;
+
+  private Vector3 velocity = Vector3.zero;
+  private Camera cm;
+
+  void Start() {
+    cm = this.GetComponent<Camera>();
+  }
+
+  void FixedUpdate() {
+    if (!target) return;
+    Vector3 point = cm.WorldToViewportPoint(target.position);
+    Vector3 delta = target.position - cm.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+    Vector3 destination = transform.position + delta;
+    transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+  }
+}
--- Assets/Scripts/ctrl.cs
+++ Assets/Scripts/ctrl.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 
 public class ctrl : MonoBehaviour {
+  public float moveSpeed = 4f;
+  public float jumpForce = 27.6f;
+
+  private bool airborne = true;
   private Animator ani;
   private Rigidbody2D rb;
   private SpriteRenderer sr;
-
-  private bool airborne = false;
 
   void Start() {
     ani = this.GetComponent<Animator>();
     rb = this.GetComponent<Rigidbody2D>();
     sr = this.GetComponent<SpriteRenderer>();
+    ani.SetBool("airborne", airborne);
+    ani.SetFloat("yvel", rb.velocity.y);
   }
 
   void OnCollisionEnter2D(Collision2D c2d) {
     if (c2d.gameObject.GetComponent(typeof(PlatformEffector2D)) != null) {
       airborne = false;
+      ani.SetBool("airborne", airborne);
+      ani.SetFloat("yvel", rb.velocity.y);
     }
   }
 
@@ -25,21 +31,20 @@ public class ctrl : MonoBehaviour {
     var v = rb.velocity;
 
     if (xdir > 0) {
-      ani.SetInteger("xdir", 1);
       sr.flipX = false;
-      rb.velocity = new Vector2(5, v.y);
+      rb.velocity = new Vector2(moveSpeed, v.y);
     } else if (xdir < 0) {
-      ani.SetInteger("xdir", -1);
       sr.flipX = true;
-      rb.velocity = new Vector2(-5, v.y);
-    } else {
-      ani.SetInteger("xdir", 0);
-      rb.velocity = new Vector2(0, v.y);
-    }
+      rb.velocity = new Vector2(-moveSpeed, v.y);
+    } else rb.velocity = new Vector2(0, v.y);
+
+    ani.SetInteger("xdir", (int) xdir);
+    ani.SetFloat("yvel", v.y);
 
     if (ydir > 0 && !airborne) {
-      rb.AddForce(new Vector2(0, 5.5f), ForceMode2D.Impulse);
+      rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
       airborne = true;
+      ani.SetBool("airborne", airborne);
     }
   }
 }
